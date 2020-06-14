96a41b3ec146d3ff8c74e27b841846051fa3c7f6
Mahesh Chandran. G <maheshchandran147@gmail.com>
2020-06-14 23:09:58 +0530

    reverted back to old implementation

--- webcam.py
+++ webcam.py
@@ -2,40 +2,54 @@ import time
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
+from core.yolov4 import YOLOv4, decode
 from PIL import Image
 import cv2
 import numpy as np
 import tensorflow as tf
 
-flags.DEFINE_string('model', None, 'path to the model')
+flags.DEFINE_string('weights', None, 'name of weights file')
 flags.DEFINE_float('score_thresh', 0.25, 'prediction score threshold')
 flags.DEFINE_float('iou_thresh', 0.213, 'iou prediction score threshold')
+flags.DEFINE_integer('size', 416, 'input size to resize the image')
+
 
 def main(argv):
     NUM_CLASS = 2
-    ANCHORS = [12,16, 19,36, 40,28, 36,75, 
-                76,55, 72,146, 142,110, 
-                192,243, 459,401]
+    ANCHORS = [12, 16, 19, 36, 40, 28, 36, 75,
+               76, 55, 72, 146, 142, 110,
+               192, 243, 459, 401]
     ANCHORS = np.array(ANCHORS, dtype=np.float32)
     ANCHORS = ANCHORS.reshape(3, 3, 2)
     STRIDES = [8, 16, 32]
-    XYSCALE = [1.2, 1.1, 1.05] 
-    model = FLAGS.model
-    input_size = int(model.split(".")[0].split("size")[1][1:])
+    XYSCALE = [1.2, 1.1, 1.05]
+    input_size = FLAGS.size
     score_thresh = FLAGS.score_thresh
     iou_thresh = FLAGS.iou_thresh
-
 
     print(f'[DEBUG][image] input_size : {input_size}')
     print(f'[DEBUG][image] score_thresh : {score_thresh}')
     print(f'[DEBUG][image] iou_thresh : {iou_thresh}')
 
-    print(f'[INFO][image] Loading {model}')
-
+    print('[INFO] Bulding Yolov4 architecture')
     tic = time.perf_counter()
-    model = tf.keras.models.load_model(model, custom_objects={'tf': tf})
+
+    input_layer = tf.keras.layers.Input([input_size, input_size, 3])
+    print(f'[INFO][image] Created input_layer of size {input_size}')
+    print(f'[DEBUG][image] input_layer : {input_layer}')
+
+    feature_maps = YOLOv4(input_layer, NUM_CLASS)
+
+    print(f'[DEBUG][image] feature_maps : {feature_maps}')
+    bbox_tensors = []
+    for i, fm in enumerate(feature_maps):
+        bbox_tensors.append(decode(fm, NUM_CLASS, i))
+
+    model = tf.keras.Model(input_layer, bbox_tensors)
+    utils.load_weights(model, FLAGS.weights)
+
     toc = time.perf_counter()
-    print('[INFO][image] Model loaded.')
+    print(f'[INFO] Architecture built.')
     print(f'[DEBUG][image] Execution took {toc - tic:0.4f} seconds')
 
     vid = cv2.VideoCapture(0)
@@ -49,17 +63,20 @@ def main(argv):
         else:
             raise ValueError("No image! Try with another video format")
         frame_size = frame.shape[:2]
-        
-        image_data = utils.image_preprocess(np.copy(frame), [input_size, input_size])
+
+        image_data = utils.image_preprocess(
+            np.copy(frame), [input_size, input_size])
         image_data = image_data[np.newaxis, ...].astype(np.float32)
         prev_time = time.time()
 
         pred_bbox = model.predict(image_data)
         print(f'[INFO][image] Finished initial predication on image')
 
-        pred_bbox = utils.postprocess_bbbox(pred_bbox, ANCHORS, STRIDES, XYSCALE)
+        pred_bbox = utils.postprocess_bbbox(
+            pred_bbox, ANCHORS, STRIDES, XYSCALE)
 
-        bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, score_thresh)
+        bboxes = utils.postprocess_boxes(
+            pred_bbox, frame_size, input_size, score_thresh)
 
         bboxes = utils.nms(bboxes, iou_thresh, method='nms')
 
@@ -68,7 +85,7 @@ def main(argv):
         curr_time = time.time()
         exec_time = curr_time - prev_time
         result = np.asarray(image)
-        info = "time: %.2f ms" %(1000*exec_time)
+        info = "time: %.2f ms" % (1000*exec_time)
 
         print(info)
 
@@ -76,7 +93,8 @@ def main(argv):
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         cv2.imshow("result", result)
         print(result.shape)
-        if cv2.waitKey(1) & 0xFF == ord('q'): break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
     vid.release()
 
 
@@ -85,5 +103,3 @@ if __name__ == '__main__':
         app.run(main)
     except SystemExit:
         pass
-
-
